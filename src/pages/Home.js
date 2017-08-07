9122686b4ee3bb40dd8d260dd4e9729134becdc1
Michael Willmott <michael@glimpse.studio>
2017-08-07 22:56:58 +0100

    Initial commit - holding page

---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+import React, { PropTypes } from 'react';
+import styled from 'styled-components';
+
+import Logo from '../assets/logo-raster.png';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const LogoContainer = styled.div`
+  flex: 0 0 auto;
+  width: 80%;
+  max-width: 340px;
+
+  img {
+    max-width: 100%;
+  }
+`;
+
+const Headline = styled.div`
+  padding: 20px;
+  text-align: center;
+  font-size: 1.2rem;
+`;
+
+const Home = () => (
+  <Container>
+    <LogoContainer>
+      <img src={Logo} />
+    </LogoContainer>
+    <Headline>
+      A glimpse into a mixed reality future. <br/>
+      Coming later in 2017
+    </Headline>
+  </Container>
+)
+
+export default Home;
